405e5d30e4cb8648736dee757d1712e384883832
Alyssa FitzGerald <lyssfitz@Alyssas-MacBook-Pro.local>
2019-04-30 16:47:55 +1000

    added lessons controller and routes

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,31 @@
+class LessonsController < ApplicationController
+    
+    def index
+        render plain: "test"
+    end
+
+    def create
+
+    end
+
+    def new
+
+    end
+
+    def show
+
+    end
+
+    def edit 
+
+    end
+
+    def update 
+
+    end
+
+    def destroy 
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get "/lessons", to: "lessons#index", as: "lessons"
+  post "/lessons", to: "lessons#create"
+  get "/lessons/new", to: "lessons#new", as: "new_lesson"
+  get "/lessons/:id", to: "lessons#show", as: "lesson"
+  put "/lessons/:id", to: "lessons#update"
+  patch "/lessons/:id", to: "lessons#update"
+  delete "/lessons/:id", to: "lessons#destroy"
+  get "/lessons/:id/edit", to: "lessons#edit", as: "edit_lesson"
 end
