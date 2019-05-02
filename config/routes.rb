f512285a8f8abdb512349ee0fb66603b50027f99
TysonEketone <tysoneketone@gmail.com>
2019-05-02 13:38:30 +1000

    Have added the custom fields to edit view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'users/registrations'}
-  # devise_for :users, path: '/', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'create_account' }
+  devise_for :users, controllers: {registrations: 'users/registrations'}, path: '/', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'create_account' }
+  get '/', to: 'pages#index', as: 'home'
 end
  
