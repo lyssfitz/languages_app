7626b8ea40583c857884ebac59f1ca00f306d862
TysonEketone <tysoneketone@gmail.com>
2019-05-09 11:47:08 +1000

    Added lesson association to orders

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :user
   belongs_to :language
+  has_many :orders
   has_many :comments, dependent: :destroy
   enum difficulty: { easy:0, intermediate: 1, expert:2 }
   validates :language, :body, :lesson_date, :lesson_time, :street, :city, :state, :postcode, :price, :max_students, :difficulty, presence: true 
